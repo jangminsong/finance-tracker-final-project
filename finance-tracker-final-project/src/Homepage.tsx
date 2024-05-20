9d12c4aeb52eb2ed6b0f346722ff7fe22b076c86
shell <shelbyarmor11@gmail.com>
2024-05-20 11:55:28 -0400

    sidenav incomplete

--- finance-tracker-final-project/src/Homepage.tsx
+++ finance-tracker-final-project/src/Homepage.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Sidenav from "./Sidenav";
 
 function Homepage() {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <div>
+        <Sidenav />
+      </div>
+      <div>hello world</div>
+    </div>
+  );
 }
 
 export default Homepage;
