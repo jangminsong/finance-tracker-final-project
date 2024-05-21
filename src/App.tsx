f1fd1522ebcdb4b4d30f5332f8fda324bfacceda
柱本珠希 <hashiramototamaki@bobcat-024-143.bates.edu>
2024-05-21 10:51:47 -0400

    tamaki added sidenav

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
 import "./App.css";
 import Homepage from "./Homepage";
+import Sidenav from "./Sidenav";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <Homepage />;
+  return (
+    <div className="App flex">
+      <Sidenav />
+      <div className="flex-grow">
+        <Homepage />
+      </div>
+    </div>
+  );
 }
 
 export default App;
